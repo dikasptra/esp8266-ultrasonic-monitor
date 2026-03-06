30db7b26a3472c566ff5a1ba046b059d280014de
Andika D.S. <andikadwisaputraa05@gmail.com>
2026-03-06 09:41:22 +0700

    Update Blynk credentials and clean up comments

--- sensor_jarak.ino
+++ sensor_jarak.ino
@@ -1,48 +1,33 @@
-/*
-  Blynk example
-  You should get Auth Token in the Blynk App.
-  You need to write the right wifiCredentials.
-*/
-
-/* Comment this out to disable prints and save space */
-#define BLYNK_TEMPLATE_ID "TMPL6w-8ZaLet"
-#define BLYNK_TEMPLATE_NAME "Jarak"
-#define BLYNK_AUTH_TOKEN "zZeCC0Q44S8EpFIxkqVs0Gq5hzvIvxWQ"
+#define BLYNK_TEMPLATE_ID "########"
+#define BLYNK_TEMPLATE_NAME "#####"
+#define BLYNK_AUTH_TOKEN "#################"
 
 #define BLYNK_PRINT Serial
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
 
-int trig_sensor_jarak = 12; // Trigger pin
-int echo_sensor_jarak = 14; // Echo pin
-int buzzer_pin = 2; // Pin untuk buzzer
-int led_pin = 5; // Pin untuk LED
+int trig_sensor_jarak = 12; 
+int echo_sensor_jarak = 14;
+int buzzer_pin = 2; 
+int led_pin = 5;
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "zZeCC0Q44S8EpFIxkqVs0Gq5hzvIvxWQ";
+char auth[] = "#############";
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "Lab Robotika"; //nama hotspot yang digunakan
-char pass[] = "lab_robotika"; //password hotspot yang digunakan
+char ssid[] = "#######"; 
+char pass[] = "########";
 
 BlynkTimer timer;
 
 void setup()
 {
-  // Debug console
   Serial.begin(9600);
   pinMode(trig_sensor_jarak, OUTPUT);
   pinMode(echo_sensor_jarak, INPUT);
-  pinMode(buzzer_pin, OUTPUT); // Set buzzer pin as output
-  pinMode(led_pin, OUTPUT); // Set LED pin as output
+  pinMode(buzzer_pin, OUTPUT); 
+  pinMode(led_pin, OUTPUT); 
 
   Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  timer.setInterval(1000L, sendSensor); // Send data every second
-  // You can also specify server:
-  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+  timer.setInterval(1000L, sendSensor); 
 }
 
 void loop()
@@ -54,36 +39,29 @@ void loop()
 void sendSensor() {
     long duration, distance;
     
-    // Clear the trigPin condition
     digitalWrite(trig_sensor_jarak, LOW);
     delayMicroseconds(2);
     
-    // Trigger the sensor
     digitalWrite(trig_sensor_jarak, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig_sensor_jarak, LOW);
     
-    // Read the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echo_sensor_jarak, HIGH);
     
-    // Calculate the distance (in cm)
     distance = duration * 0.034 / 2;
     
-    // Print distance to Serial Monitor
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.println(" cm");
     
-    // Send distance data to Blynk
-    Blynk.virtualWrite(V14, distance); // Use V0 for a display widget in your Blynk app
+    Blynk.virtualWrite(V14, distance);
 
-    // Check if distance is less than 10 cm
      if (distance < 10) {
-        digitalWrite(buzzer_pin, HIGH); // Turn on buzzer
-        digitalWrite(led_pin, HIGH);    // Turn on LED
+        digitalWrite(buzzer_pin, HIGH);
+        digitalWrite(led_pin, HIGH);   
         Blynk.logEvent("notif_sensor");
     } else {
-        digitalWrite(buzzer_pin, LOW); // Turn off buzzer
-        digitalWrite(led_pin, LOW);    // Turn off LED
+        digitalWrite(buzzer_pin, LOW);
+        digitalWrite(led_pin, LOW);   
     }
 } 
